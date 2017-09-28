f9c2a8e158cf52da09d15de970de19e225bd8bd4
Administrator <137148873@qq.com>
2017-09-28 11:08:58 +0800

    完善单页面笔记应用代码

---
+++ vueNote/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <toolbar></toolbar>
+    <note-list></note-list>
+    <editor></editor>
+  </div>
+</template>
+
+<script>
+  import Toolbar from './components/Toolbar'
+  import NoteList from './components/NoteList'
+  import Editor from './components/Editor'
+
+  export default {
+    components: {
+      Toolbar,
+      NoteList,
+      Editor
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
